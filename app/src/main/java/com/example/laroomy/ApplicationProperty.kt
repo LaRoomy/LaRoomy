394d7376251d57a373f6ad71d8fb5dee136b0300
Hans Philipp Zimmermann <hans.zimmermann@laroomy.de>
2020-03-30 12:55:27 +0200

    Update Work 30/03/2020

--- app/src/main/java/com/example/laroomy/ApplicationProperty.kt
+++ app/src/main/java/com/example/laroomy/ApplicationProperty.kt
@@ -7,6 +7,12 @@ const val ERROR_MESSAGE = 0
 const val WARNING_MESSAGE = 1
 const val INFO_MESSAGE = 2
 
+const val UNDEFINED_ELEMENT = 0
+const val PROPERTY_ELEMENT = 1
+const val GROUP_ELEMENT = 2
+const val SEPARATOR_ELEMENT = 3
+const val NO_CONTENT_ELEMENT = 4
+
 class ApplicationProperty : Application() {
 
     companion object {
@@ -24,6 +30,37 @@ class ApplicationProperty : Application() {
     }
 }
 
+fun resourceIdForImageId(imageID: Int): Int {
+    return when (imageID) {
+        -1 -> R.drawable.image_error_state
+        0 -> R.drawable.image_error_state
+        1 -> R.drawable.placeholder_blue_white
+        2 -> R.drawable.rgb_control_colored
+        3 -> R.drawable.lightbulb_darkblue
+        4 -> R.drawable.lightbulb_lightblue
+        5 -> R.drawable.lightbulb_monocrom
+        6 -> R.drawable.lightbulb_scheme
+        7 -> R.drawable.sun_yellow
+        8 -> R.drawable.sun_white
+        9 -> R.drawable.sun_blue
+        10 -> R.drawable.sun_blue_monocrom
+        else -> 4
+    }
+}
+
+class DevicePropertyListContentInformation{
+    var canNavigateForward = false
+    var elementType = SEPARATOR_ELEMENT
+    var elementIndex = -1
+    var elementText = ""
+    var elementID = -1
+    var imageID = -1
+}
+
 interface OnItemClickListener {
     fun onItemClicked(index: Int, data: LaRoomyDevicePresentationModel)
+}
+
+interface OnPropertyClickListener{
+    fun onPropertyClicked(index: Int, data: DevicePropertyListContentInformation)
 }
--- app/src/main/java/com/example/laroomy/DeviceMainActivity.kt
+++ app/src/main/java/com/example/laroomy/DeviceMainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.laroomy
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_device_main.*
 
-class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCallback, BLEConnectionManager.BleEventCallback {
+
+class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCallback, BLEConnectionManager.BleEventCallback, OnPropertyClickListener {
 
     private var isUpToDate = false
+
+    // device property list elements
+    private lateinit var devicePropertyListRecyclerView: RecyclerView
+    private lateinit var devicePropertyListViewAdapter: RecyclerView.Adapter<*>
+    private lateinit var devicePropertyListLayoutManager: RecyclerView.LayoutManager
+    private var devicePropertyList= ArrayList<DevicePropertyListContentInformation>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,14 +31,26 @@ class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCal
 
         ApplicationProperty.bluetoothConnectionManger.reAlignContextObjects(this, this@DeviceMainActivity, this)
         ApplicationProperty.bluetoothConnectionManger.setPropertyEventHandler(this)
-        //(this.applicationContext as ApplicationProperty).bluetoothConnectionManger.reAlignContextObjects(this, this@DeviceMainActivity, this)
-        //(this.applicationContext as ApplicationProperty).bluetoothConnectionManger.setPropertyEventHandler(this)
 
         // init recycler view!!
+        this.devicePropertyListLayoutManager = LinearLayoutManager(this)
 
+        // set empty list placeholder in array-list
+        var dc = DevicePropertyListContentInformation()
+        dc.elementType = NO_CONTENT_ELEMENT
+        this.devicePropertyList.add(dc)
 
+        // bind array to adapter
+        this.devicePropertyListViewAdapter = devicePropertyListAdapter(this.devicePropertyList, this, this@DeviceMainActivity)
 
-
+        // bind the elements to the recycler
+        this.devicePropertyListRecyclerView =
+            findViewById<RecyclerView>(R.id.devicePropertyListView)
+                .apply {
+                    setHasFixedSize(true)
+                    layoutManager = devicePropertyListLayoutManager
+                    adapter = devicePropertyListViewAdapter
+                }
     }
 
     override fun onPause() {
@@ -35,12 +63,16 @@ class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCal
 
     override fun onResume() {
         super.onResume()
-        setUIConnectionStatus(ApplicationProperty.bluetoothConnectionManger.isConnected) //TODO: parameter is reset in new activity -> why???
+        setUIConnectionStatus(ApplicationProperty.bluetoothConnectionManger.isConnected)
         this.isUpToDate =
             ApplicationProperty.bluetoothConnectionManger.isPropertyUpToDate
         if(this.isUpToDate){
             this.adaptUIToPropertyListing()
         }
+    }
+
+    override fun onPropertyClicked(index: Int, data: DevicePropertyListContentInformation) {
+
     }
 
     fun onDiscardDeviceButtonClick(@Suppress("UNUSED_PARAMETER")view: View){
@@ -79,6 +111,80 @@ class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCal
 
         if(!this.isUpToDate){
             this.adaptUIToPropertyListing()
+        }
+    }
+
+    // device property list adapter:
+    class devicePropertyListAdapter(
+        private val devicePropertyAdapter: ArrayList<DevicePropertyListContentInformation>,
+        private val itemClickListener: OnPropertyClickListener,
+        private val activityContext: Context
+    ) : RecyclerView.Adapter<devicePropertyListAdapter.DPLViewHolder>() {
+
+        class DPLViewHolder(val linearLayout: LinearLayout)
+            : RecyclerView.ViewHolder(linearLayout) {
+
+            fun bind(data: DevicePropertyListContentInformation, itemClick: OnPropertyClickListener, position: Int){
+                itemClick.onPropertyClicked(position, data)
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DPLViewHolder {
+            val linearLayout =
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.device_property_list_element, parent, false) as LinearLayout
+            return DPLViewHolder(linearLayout)
+        }
+
+        override fun onBindViewHolder(holder: DPLViewHolder, position: Int) {
+
+            when(devicePropertyAdapter.elementAt(position).elementType){
+                UNDEFINED_ELEMENT -> {
+                    // will be treated like a separator!
+                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
+                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
+                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
+                }
+                GROUP_ELEMENT -> {
+                    // group elements are for subclassing properties, but cannot navigate forward (by now...)
+                    holder.linearLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.GONE
+                    // set the image requested by the device (or a placeholder)
+                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(
+                        resourceIdForImageId(devicePropertyAdapter.elementAt(position).imageID)
+                    )
+                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = devicePropertyAdapter.elementAt(position).elementText
+                    // make sure it is visible (TODO: is this really necessary??)
+                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.VISIBLE
+
+                }
+                PROPERTY_ELEMENT -> {
+                    if(devicePropertyAdapter.elementAt(position).canNavigateForward)
+                        holder.linearLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.VISIBLE
+                    else  holder.linearLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.GONE
+
+                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(
+                        resourceIdForImageId(devicePropertyAdapter.elementAt(position).imageID)
+                    )
+                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = devicePropertyAdapter.elementAt(position).elementText
+                    // make sure it is visible (TODO: is this really necessary??)
+                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.VISIBLE
+                }
+                SEPARATOR_ELEMENT -> {
+                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
+                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
+                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
+                }
+                else -> {
+                    // will be treated like a separator!
+                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
+                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
+                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
+                }
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return devicePropertyAdapter.size
         }
     }
 }
