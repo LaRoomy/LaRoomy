949c265388740b3809696563844bd48061d3f9fb
Hans Philipp Zimmermann <hans.zimmermann@laroomy.de>
2020-03-31 21:12:53 +0200

    Update Home 31/03/2020

--- app/src/main/java/com/example/laroomy/DeviceMainActivity.kt
+++ app/src/main/java/com/example/laroomy/DeviceMainActivity.kt
@@ -3,6 +3,8 @@ package com.example.laroomy
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginBottom
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -35,7 +38,7 @@ class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCal
         this.devicePropertyListLayoutManager = LinearLayoutManager(this)
 
         // set empty list placeholder in array-list
-        var dc = DevicePropertyListContentInformation()
+        val dc = DevicePropertyListContentInformation()
         dc.elementType = NO_CONTENT_ELEMENT
         this.devicePropertyList.add(dc)
 
@@ -86,60 +89,74 @@ class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCal
     }
 
     private fun adaptUIToPropertyListing(){
-        // erase the recycler list
-        this.devicePropertyList.clear()
-        // create the new list -> start with the groups
-        if(ApplicationProperty.bluetoothConnectionManger.laRoomyPropertyGroupList.size > 0){
-            for(laRoomyDevicePropertyGroup in ApplicationProperty.bluetoothConnectionManger.laRoomyPropertyGroupList){
-                // create the group entry
-                val dpl = DevicePropertyListContentInformation()
-                dpl.elementType = GROUP_ELEMENT
-                dpl.canNavigateForward = false
-                dpl.elementID = laRoomyDevicePropertyGroup.groupID
-                dpl.elementText = laRoomyDevicePropertyGroup.groupName
-                dpl.imageID = laRoomyDevicePropertyGroup.imageID
-                // add the group to the list
-                this.devicePropertyList.add(dpl)
-                // add the device properties to the group by their IDs
-                for(ID in laRoomyDevicePropertyGroup.memberIDs){
-                    ApplicationProperty.bluetoothConnectionManger.laRoomyDevicePropertyList.forEachIndexed { index, laRoomyDeviceProperty ->
-                        if(laRoomyDeviceProperty.propertyID == ID){
-                            // ID found -> add property to list
-                            val propertyEntry = DevicePropertyListContentInformation()
-                            propertyEntry.elementType = PROPERTY_ELEMENT
-                            propertyEntry.canNavigateForward = laRoomyDeviceProperty.needNavigation()
-                            propertyEntry.elementText = laRoomyDeviceProperty.propertyDescriptor
-                            propertyEntry.imageID = laRoomyDeviceProperty.imageID
-                            propertyEntry.elementID = laRoomyDeviceProperty.propertyID
-                            propertyEntry.elementIndex = index
-                            // add it to the list
-                            this.devicePropertyList.add(propertyEntry)
-                            // ID found -> further processing not necessary -> break the loop
-                            return@forEachIndexed
+
+//        Looper.prepare()
+//        Handler().postDelayed({
+//            // TODO: error!
+
+        Thread(
+            Runnable {
+
+            // erase the recycler list
+            this.devicePropertyList.clear()
+            // create the new list -> start with the groups
+            if (ApplicationProperty.bluetoothConnectionManger.laRoomyPropertyGroupList.size > 0) {
+                for (laRoomyDevicePropertyGroup in ApplicationProperty.bluetoothConnectionManger.laRoomyPropertyGroupList) {
+                    // create the group entry
+                    val dpl = DevicePropertyListContentInformation()
+                    dpl.elementType = GROUP_ELEMENT
+                    dpl.canNavigateForward = false
+                    dpl.elementID = laRoomyDevicePropertyGroup.groupID
+                    dpl.elementText = laRoomyDevicePropertyGroup.groupName
+                    dpl.imageID = laRoomyDevicePropertyGroup.imageID
+                    // add the group to the list
+                    this.devicePropertyList.add(dpl)
+                    // add the device properties to the group by their IDs
+                    for (ID in laRoomyDevicePropertyGroup.memberIDs) {
+                        ApplicationProperty.bluetoothConnectionManger.laRoomyDevicePropertyList.forEachIndexed { index, laRoomyDeviceProperty ->
+                            if (laRoomyDeviceProperty.propertyID == ID) {
+                                // ID found -> add property to list
+                                val propertyEntry = DevicePropertyListContentInformation()
+                                propertyEntry.elementType = PROPERTY_ELEMENT
+                                propertyEntry.canNavigateForward =
+                                    laRoomyDeviceProperty.needNavigation()
+                                propertyEntry.elementText = laRoomyDeviceProperty.propertyDescriptor
+                                propertyEntry.imageID = laRoomyDeviceProperty.imageID
+                                propertyEntry.elementID = laRoomyDeviceProperty.propertyID
+                                propertyEntry.elementIndex = index
+                                // add it to the list
+                                this.devicePropertyList.add(propertyEntry)
+                                // ID found -> further processing not necessary -> break the loop
+                                return@forEachIndexed
+                            }
                         }
                     }
+                    val dpl2 = DevicePropertyListContentInformation()
+                    dpl2.elementType = SEPARATOR_ELEMENT
+                    dpl2.canNavigateForward = false
+                    this.devicePropertyList.add(dpl2)
                 }
             }
-        }
-        // now add the properties which are not part of a group
-        //for(laRoomyDeviceProperty in ApplicationProperty.bluetoothConnectionManger.laRoomyDevicePropertyList){
-        ApplicationProperty.bluetoothConnectionManger.laRoomyDevicePropertyList.forEachIndexed { index, laRoomyDeviceProperty ->
-            // only add the non-group properties
-            if(!laRoomyDeviceProperty.isGroupMember){
-                // create the entry
-                val propertyEntry = DevicePropertyListContentInformation()
-                propertyEntry.elementType = PROPERTY_ELEMENT
-                propertyEntry.canNavigateForward = laRoomyDeviceProperty.needNavigation()
-                propertyEntry.elementID = laRoomyDeviceProperty.propertyID
-                propertyEntry.imageID = laRoomyDeviceProperty.imageID
-                propertyEntry.elementText = laRoomyDeviceProperty.propertyDescriptor
-                propertyEntry.elementIndex = index
-                // add it to the list
-                this.devicePropertyList.add(propertyEntry)
+            // now add the properties which are not part of a group
+            //for(laRoomyDeviceProperty in ApplicationProperty.bluetoothConnectionManger.laRoomyDevicePropertyList){
+            ApplicationProperty.bluetoothConnectionManger.laRoomyDevicePropertyList.forEachIndexed { index, laRoomyDeviceProperty ->
+                // only add the non-group properties
+                if (!laRoomyDeviceProperty.isGroupMember) {
+                    // create the entry
+                    val propertyEntry = DevicePropertyListContentInformation()
+                    propertyEntry.elementType = PROPERTY_ELEMENT
+                    propertyEntry.canNavigateForward = laRoomyDeviceProperty.needNavigation()
+                    propertyEntry.elementID = laRoomyDeviceProperty.propertyID
+                    propertyEntry.imageID = laRoomyDeviceProperty.imageID
+                    propertyEntry.elementText = laRoomyDeviceProperty.propertyDescriptor
+                    propertyEntry.elementIndex = index
+                    // add it to the list
+                    this.devicePropertyList.add(propertyEntry)
+                }
             }
-        }
-        // update the recycler
-        this.devicePropertyListViewAdapter.notifyDataSetChanged()
+            // update the recycler
+            this.devicePropertyListViewAdapter.notifyDataSetChanged()
+        }).start()
     }
 
     private fun setUIConnectionStatus(status :Boolean){
@@ -180,8 +197,8 @@ class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCal
         private val activityContext: Context
     ) : RecyclerView.Adapter<DevicePropertyListAdapter.DPLViewHolder>() {
 
-        class DPLViewHolder(val linearLayout: LinearLayout)
-            : RecyclerView.ViewHolder(linearLayout) {
+        class DPLViewHolder(val constraintLayout: ConstraintLayout)
+            : RecyclerView.ViewHolder(constraintLayout) {
 
             fun bind(data: DevicePropertyListContentInformation, itemClick: OnPropertyClickListener, position: Int){
                 itemClick.onPropertyClicked(position, data)
@@ -189,10 +206,10 @@ class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCal
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DPLViewHolder {
-            val linearLayout =
+            val constraintLayout =
                 LayoutInflater.from(parent.context)
-                    .inflate(R.layout.device_property_list_element, parent, false) as LinearLayout
-            return DPLViewHolder(linearLayout)
+                    .inflate(R.layout.device_property_list_element, parent, false) as ConstraintLayout
+            return DPLViewHolder(constraintLayout)
         }
 
         override fun onBindViewHolder(holder: DPLViewHolder, position: Int) {
@@ -200,44 +217,58 @@ class DeviceMainActivity : AppCompatActivity(), BLEConnectionManager.PropertyCal
             when(devicePropertyAdapter.elementAt(position).elementType){
                 UNDEFINED_ELEMENT -> {
                     // will be treated like a separator!
-                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
-                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
-                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
+                    //holder.constraintLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
+                    holder.constraintLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
+                    holder.constraintLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
                 }
                 GROUP_ELEMENT -> {
+                    // make the element higher than the properties
+                    //holder.constraintLayout.minHeight = 120
+                    // TODO: make it higher
+
+//                    val lParams = holder.constraintLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).layoutParams as ConstraintLayout.LayoutParams
+//                    lParams.setMargins(0,20,0,20)
+
+
                     // group elements are for subclassing properties, but cannot navigate forward (by now...)
-                    holder.linearLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.GONE
+                    holder.constraintLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.GONE
                     // set the image requested by the device (or a placeholder)
-                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(
+                    holder.constraintLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(
                         resourceIdForImageId(devicePropertyAdapter.elementAt(position).imageID)
                     )
-                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = devicePropertyAdapter.elementAt(position).elementText
+                    holder.constraintLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = devicePropertyAdapter.elementAt(position).elementText
                     // make sure it is visible (TODO: is this really necessary??)
-                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.VISIBLE
+                    holder.constraintLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.VISIBLE
 
                 }
                 PROPERTY_ELEMENT -> {
                     if(devicePropertyAdapter.elementAt(position).canNavigateForward)
-                        holder.linearLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.VISIBLE
-                    else  holder.linearLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.GONE
+                        holder.constraintLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.VISIBLE
+                    else  holder.constraintLayout.findViewById<ImageView>(R.id.forwardImage).visibility = View.GONE
 
-                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(
+                    holder.constraintLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(
                         resourceIdForImageId(devicePropertyAdapter.elementAt(position).imageID)
                     )
-                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = devicePropertyAdapter.elementAt(position).elementText
+                    holder.constraintLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = devicePropertyAdapter.elementAt(position).elementText
                     // make sure it is visible (TODO: is this really necessary??)
-                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.VISIBLE
+                    holder.constraintLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.VISIBLE
                 }
                 SEPARATOR_ELEMENT -> {
-                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
-                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
-                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
+
+                    //holder.constraintLayout.findViewById<View>(R.id.topSeparator).bottom = 10
+                    //holder.constraintLayout.findViewById<View>(R.id.bottomSeparator).top = 10
+
+                    // TODO: make it better!
+
+                    //holder.constraintLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
+                    holder.constraintLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
+                    holder.constraintLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
                 }
                 else -> {
                     // will be treated like a separator!
-                    holder.linearLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
-                    holder.linearLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
-                    holder.linearLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
+                    //holder.constraintLayout.findViewById<ImageView>(R.id.devicePropertyIdentificationImage).setBackgroundResource(R.drawable.placeholder_blue_white)
+                    holder.constraintLayout.findViewById<TextView>(R.id.devicePropertyNameTextView).text = this.activityContext.getString(R.string.LaRoomyDevicePropertyNamePlaceholder)
+                    holder.constraintLayout.findViewById<ConstraintLayout>(R.id.contentHolderLayout).visibility = View.GONE
                 }
             }
         }
